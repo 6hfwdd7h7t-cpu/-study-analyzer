cd7a83e735ff2c22f9b3c934371eda91593457f8
6hfwdd7h7t-cpu <6hfwdd7h7t@privaterelay.appleid.com>
2026-09-12 01:23:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="study-analyzer-v3-5";
+const CACHE="study-analyzer-v4-0";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg","./apple-touch-icon.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
